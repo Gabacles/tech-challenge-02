d7c522f54219ebdb2bdab0617a32434b66d8a9b4
Gabacles <g.dalrovere@gmail.com>
2024-08-13 15:22:09 -0300

    feat: add swagger doc in posts routes

--- src/routes/posts.routes.ts
+++ src/routes/posts.routes.ts
@@ -8,47 +8,189 @@ export async function postRoutes(fastify: FastifyInstance) {
 
   fastify.addHook("preHandler", authMiddleware);
 
-  fastify.get("/", async (req, reply) => {
-    try {
-      const data = await postUseCase.list();
+  fastify.get(
+    "/",
+    {
+      schema: {
+        description: "List all posts",
+        tags: ["Posts"],
+        summary: "List all posts",
+        headers: {
+          type: "object",
+          properties: {
+            email: { type: "string" },
+          },
+          required: ["email"],
+        },
+        response: {
+          200: {
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                id: { type: "number" },
+                title: { type: "string" },
+                content: { type: "string" },
+                user_email: { type: "string" },
+              },
+            },
+          },
+        },
+      },
+    },
+    async (req, reply) => {
+      try {
+        const data = await postUseCase.list();
 
-      return reply.code(200).send(data);
-    } catch (error: any) {
-      reply.code(500).send({ error: error.message });
+        return reply.code(200).send(data);
+      } catch (error: any) {
+        reply.code(500).send({ error: error.message });
+      }
     }
-  });
+  );
 
-  fastify.get<{ Params: { id: number } }>("/:id", async (req, reply) => {
-    const { id } = req.params;
-    try {
-      const data = await postUseCase.findById(id);
+  fastify.get<{ Params: { id: number } }>(
+    "/:id",
+    {
+      schema: {
+        description: "Find post by id",
+        tags: ["Posts"],
+        summary: "Find post by id",
+        headers: {
+          type: "object",
+          properties: {
+            email: { type: "string" },
+          },
+          required: ["email"],
+        },
+        params: {
+          type: "object",
+          properties: {
+            id: { type: "number" },
+          },
+          required: ["id"],
+        },
+        response: {
+          200: {
+            type: "object",
+            properties: {
+              id: { type: "number" },
+              title: { type: "string" },
+              content: { type: "string" },
+              user_email: { type: "string" },
+            },
+          },
+        },
+      },
+    },
+    async (req, reply) => {
+      const { id } = req.params;
+      try {
+        const data = await postUseCase.findById(id);
 
-      return reply.code(200).send(data);
-    } catch (error: any) {
-      reply.code(500).send({ error: error.message });
+        return reply.code(200).send(data);
+      } catch (error: any) {
+        reply.code(500).send({ error: error.message });
+      }
     }
-  });
+  );
 
-  fastify.post<{ Body: PostCreate }>("/", async (req, reply) => {
-    const { title, content } = req.body;
+  fastify.post<{ Body: PostCreate }>(
+    "/",
+    {
+      schema: {
+        description: "Create a post",
+        tags: ["Posts"],
+        summary: "Create a post",
+        body: {
+          type: "object",
+          properties: {
+            title: { type: "string" },
+            content: { type: "string" },
+          },
+          required: ["title", "content"],
+        },
+        headers: {
+          type: "object",
+          properties: {
+            email: { type: "string" },
+          },
+          required: ["email"],
+        },
+        response: {
+          201: {
+            type: "object",
+            properties: {
+              id: { type: "number" },
+              title: { type: "string" },
+              content: { type: "string" },
+              user_email: { type: "string" },
+            },
+          },
+        },
+      },
+    },
+    async (req, reply) => {
+      const { title, content } = req.body;
 
-    const { email } = req.headers;
+      const { email } = req.headers;
 
-    try {
-      const data = await postUseCase.create({
-        title,
-        content,
-        user_email: email,
-      });
+      try {
+        const data = await postUseCase.create({
+          title,
+          content,
+          user_email: email,
+        });
 
-      return reply.code(201).send(data);
-    } catch (error: any) {
-      reply.code(500).send({ error: error.message });
+        return reply.code(201).send(data);
+      } catch (error: any) {
+        reply.code(500).send({ error: error.message });
+      }
     }
-  });
+  );
 
   fastify.put<{ Body: PostCreate; Params: { id: number } }>(
     "/:id",
+    {
+      schema: {
+        description: "Update a post",
+        tags: ["Posts"],
+        summary: "Update a post",
+        body: {
+          type: "object",
+          properties: {
+            title: { type: "string" },
+            content: { type: "string" },
+          },
+          required: ["title", "content"],
+        },
+        headers: {
+          type: "object",
+          properties: {
+            email: { type: "string" },
+          },
+          required: ["email"],
+        },
+        params: {
+          type: "object",
+          properties: {
+            id: { type: "number" },
+          },
+          required: ["id"],
+        },
+        response: {
+          200: {
+            type: "object",
+            properties: {
+              id: { type: "number" },
+              title: { type: "string" },
+              content: { type: "string" },
+              user_email: { type: "string" },
+            },
+          },
+        },
+      },
+    },
     async (req, reply) => {
       const { title, content } = req.body;
       const { id } = req.params;
@@ -63,19 +205,89 @@ export async function postRoutes(fastify: FastifyInstance) {
     }
   );
 
-  fastify.delete<{ Params: { id: number } }>("/:id", async (req, reply) => {
-    const { id } = req.params;
-    try {
-      const data = await postUseCase.delete(id);
+  fastify.delete<{ Params: { id: number } }>(
+    "/:id",
+    {
+      schema: {
+        description: "Delete a post",
+        tags: ["Posts"],
+        summary: "Delete a post",
+        params: {
+          type: "object",
+          properties: {
+            id: { type: "number" },
+          },
+          required: ["id"],
+        },
+        headers: {
+          type: "object",
+          properties: {
+            email: { type: "string" },
+          },
+          required: ["email"],
+        },
+        response: {
+          200: {
+            type: "object",
+            properties: {
+              id: { type: "number" },
+              title: { type: "string" },
+              content: { type: "string" },
+              user_email: { type: "string" },
+            },
+          },
+        },
+      },
+    },
+    async (req, reply) => {
+      const { id } = req.params;
+      try {
+        const data = await postUseCase.delete(id);
 
-      return reply.code(200).send(data);
-    } catch (error: any) {
-      reply.code(500).send({ error: error.message });
+        return reply.code(200).send(data);
+      } catch (error: any) {
+        reply.code(500).send({ error: error.message });
+      }
     }
-  });
+  );
 
   fastify.get<{ Querystring: { search: string } }>(
     "/search",
+    {
+      schema: {
+        description: "Search post by title or content",
+        tags: ["Posts"],
+        summary: "Search post by title or content",
+        querystring: {
+          type: "object",
+          properties: {
+            search: { type: "string" },
+          },
+          required: ["search"],
+        },
+        headers: {
+          type: "object",
+          properties: {
+            email: { type: "string" },
+          },
+          required: ["email"],
+        },
+        response: {
+          200: {
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                id: { type: "number" },
+                title: { type: "string" },
+                content: { type: "string" },
+                user_email: { type: "string" },
+              },
+            },
+          },
+        },
+      },
+    },
     async (req, reply) => {
       const { search } = req.query;
       try {
