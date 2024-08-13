18bb824005f494a197e00a23160ff079b37c952d
Gabacles <g.dalrovere@gmail.com>
2024-08-13 15:21:06 -0300

    chore: Add fastify swagger documentation and UI

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,35 @@
 import fastify, { FastifyInstance } from "fastify";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 import { userRoutes } from "./routes/users.routes";
 import { postRoutes } from "./routes/posts.routes";
 
 const app: FastifyInstance = fastify({ logger: true });
+
+const swaggerOptions = {
+  swagger: {
+    info: {
+      title: "Tech Challenge II API",
+      description: "API Documentation",
+      version: "1.0.0",
+    },
+    host: "localhost",
+    schemes: ["http", "https"],
+    consumes: ["application/json"],
+    produces: ["application/json"],
+    tags: [{ name: "Default", description: "Default" }],
+  },
+};
+
+const swaggerUiOptions = {
+  routePrefix: "/documentation",
+  swaggerOptions: {
+    url: "/documentation/json",
+  },
+};
+
+app.register(fastifySwagger, swaggerOptions);
+app.register(fastifySwaggerUi, swaggerUiOptions);
 
 app.register(userRoutes, { prefix: "/users" });
 app.register(postRoutes, { prefix: "/posts" });
